f651f1513e8c5e46a2f88eee2d275dfa18a45703
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-29 12:13:25 +0530

    doing header section

--- my-youtube/src/components/Head.js
+++ my-youtube/src/components/Head.js
@@ -1,8 +1,36 @@
 import React from "react";
 const Head = () => {
   return (
-    <div>
-      <h1>Head</h1>
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-8"
+          src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+          alt="hamburger"
+        />
+        <img
+          className="h-8 mx-2"
+          src="https://vectorseek.com/wp-content/uploads/2021/01/YouTube-Logo-Vector.png"
+          alt="youtube logo"
+        />
+      </div>
+      <div className="col-span-10 px-10">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+          placeholder="Search"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-200">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="user-icon"
+        />
+      </div>
     </div>
   );
 };
